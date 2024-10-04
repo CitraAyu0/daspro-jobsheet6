45cd0bea2fa78d1815d5a5a2210c63ecfe6b7f81
CitraAyu0 <meilindacitraayu@gmail.com>
2024-10-04 12:22:21 +0700

    update percobaan2

--- Pemilihan2Percobaan2_05.java
+++ Pemilihan2Percobaan2_05.java
@@ -1,24 +1,72 @@
 import java.util.Scanner;
 public class Pemilihan2Percobaan2_05 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner input05 = new Scanner (System.in);
 
-        System.out.print("Masukkan sudut 1: ");
-        float sudut1 = sc.nextFloat();
-        System.out.print("Masukkan sudut 2: ");
-        float sudut2 = sc.nextFloat();
-        System.out.print("Masukkan sudut 3: ");
-        float sudut3 = sc.nextFloat();
+        String member; 
+        double diskon, totalHarga, harga;
+        int jumlah,menu;
 
-        float totalSudut = sudut1 + sudut2 + sudut3;
+        System.out.println("-----------------------");
+        System.out.println("=====MENU KAFE JTI=====");
+        System.out.println("-----------------------");
+        System.out.println("1. Ricebowl");
+        System.out.println("2. Ice tea");
+        System.out.println("3. Paket Bundling (Ricebowl + Ice tea)");
+        System.out.println("---------------------------------------");
+        System.out.print("Masukkan angka dari menu yang dipilih = ");
+        menu = input05.nextInt();
+        input05.nextLine();
+        System.out.print("Apakah punya member y/n? = ");
+        member = input05.nextLine();
+        System.out.println("---------------------------------------");
+    
+    if (member.equalsIgnoreCase("y")) {
+        diskon = 0.10;
+        System.out.println("Besar diskon = 10 %");
+        if (menu == 1) {
+            harga = 14000;
+            System.out.println("Harga Ricebowl =" + harga);
 
-        if (totalSudut == 180) {
-            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
-                System.out.println("Segitiga tersebut adalah segitiga siku siku");
-            }else 
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku siku");
-        } else
-            System.err.println("Bukan segitiga");
-        }
+        }else if (menu == 2) {
+            harga = 3000;
+            System.out.println("Harga ice tea = " + harga);
+
+        }else if (menu == 3) {
+            harga = 15000;
+            System.out.println("Harga bundling =" + harga);
+
+        }else{
+            System.out.println("Masukkan pilihan menu dengan benar");
+            return;
+        }    
+        totalHarga = harga - (harga * diskon);
+        System.out.println("Total bayar setelah diskon =" + totalHarga);
     }
+    else if (member.equalsIgnoreCase("n")) {
+        if (menu == 1) {
+            harga = 14000;
+            System.out.println("Harga ricebowl =" + harga);
 
+        }else if (menu == 2) {
+            harga = 3000;
+            System.out.println("Harga ice tea =" + harga);
+
+        }else if (menu == 3) {
+            harga = 15000;
+            System.out.println("Harga bundling =" + harga);
+
+        }else {
+            System.out.println("Masukkan pilihan menu dengan benar");
+            return;
+        }
+        System.out.println("Total harga = " + harga);
+
+    }else{
+        System.out.println("Member tidak valid");
+    }
+    System.out.println("-------------------------------");
+}
+}
+
+        
