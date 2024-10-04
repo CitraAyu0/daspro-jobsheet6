acbac80dd10d0d54a78cc27f405b805a87f419be
CitraAyu0 <meilindacitraayu@gmail.com>
2024-10-04 09:51:06 +0700

    modif tahun 2100

--- Pemilihan2Percobaan1_05.java
+++ Pemilihan2Percobaan1_05.java
@@ -8,10 +8,19 @@ public class Pemilihan2Percobaan1_05{
         int tahun = input05.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
+            if ((tahun % 100) != 0)
                 System.out.println("Tahun Kabisat");
         } else 
             System.out.println("Bukan Tahun Kabisat");
+        
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) == 0) {
+                System.out.println(" Bukan Tahun Kabisat");
+            }
+        }
         }
     }
-}
+
+        
+    
+
