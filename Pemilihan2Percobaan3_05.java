fd7e5eb3863592e744deb40a3347afb7fc437174
CitraAyu0 <meilindacitraayu@gmail.com>
2024-10-04 22:49:38 +0700

    penambahan tugas1 bilangan max

--- Pemilihan2Percobaan3_05.java
+++ Pemilihan2Percobaan3_05.java
@@ -13,7 +13,7 @@ public class Pemilihan2Percobaan3_05 {
         System.out.print("Masukkan Besarnya Penghasilan: ");
         penghasilan = input05.nextInt();
 
-        if (kategori.equalsIgnoreCase("pekerja")) {
+        if (kategori.equals("pekerja")) {
             if (penghasilan <= 2000000) {
                 pajak = 0.1;
             }else if (penghasilan <= 3000000) {
@@ -24,7 +24,7 @@ public class Pemilihan2Percobaan3_05 {
             gajiBersih = (int) (penghasilan - (pajak*penghasilan));
             System.out.print("Penghasil Bersih: " + gajiBersih);
             
-        }else if (kategori.equalsIgnoreCase("Pebisnis")) {
+        }else if (kategori.equals("Pebisnis")) {
             if (penghasilan <= 2500000) {
                 pajak = 0.15;
             }else if (penghasilan <= 3500000) {
